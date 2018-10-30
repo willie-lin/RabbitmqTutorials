cb62dbad3a75398c3deaac308b1e043831c36c48
Willie-lin <yyhmmwan@gmail.com>
2018-10-31 00:37:35 +0800

    Initial commit

---
+++ src/main/java/com/rabbitmq/rabbitmqtutorial/RabbitmqtutorialApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitmq.rabbitmqtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqtutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqtutorialApplication.class, args);
+    }
+}
