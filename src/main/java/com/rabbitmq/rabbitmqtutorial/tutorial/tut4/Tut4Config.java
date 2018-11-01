86d5644e9b9ddea6f705d66f62bc8c5063374fc9
Willie-lin <yyhmmwan@gmail.com>
2018-11-01 09:57:52 +0800

    add tut4

---
+++ src/main/java/com/rabbitmq/rabbitmqtutorial/tutorial/tut4/Tut4Config.java
@@ -0,0 +1,17 @@
+package com.rabbitmq.rabbitmqtutorial.tutorial.tut4;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package: com.rabbitmq.rabbitmqtutorial.tutorial.tut4
+ * @auther: YuAn
+ * @Date: 2018/11/1
+ * @Time: 9:29
+ * @Project_name: RabbitmqTutorial
+ * To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+public class Tut4Config {
+
+
+}
